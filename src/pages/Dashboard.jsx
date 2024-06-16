280818932bf8fb45da790b11a879c8412f99588e
Mario Youssef <marioyoussef936@gmail.com>
2024-06-16 22:32:58 +0300

    fixed image size

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,40 +5,24 @@ function Dashboard() {
   const Data = useSelector((state) => state.account.data.user);
   const FullName = Data.user_metadata.FullName;
   const pic = useSelector((state) => state.account.image);
-    console.log(`This is the image: ${pic}`)
   return (
     <div className="flex h-screen bg-gray-100">
       <SideBar />
 
       <div className="flex-1 flex flex-col">
         {/* Header */}
-        <header className="flex items-center justify-between h-16 bg-white shadow px-4">
+        <header className="flex items-center justify-between h-24 bg-white shadow px-4">
           <div className="font-bold text-lg">Welcome Back {FullName}</div>
+        
+           
+       
+          <div className="relative flex items-center justify-center h-full">
           <img
               src={pic}
               alt="Profile"
-              className="w-10 h-10 rounded-full mr-2"
+              className="w-20 h-20 rounded-full mr-5"
             />
            
-       
-          <div className="relative">
-            
-            <input
-              type="text"
-              className="pl-10 pr-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
-              placeholder="Search..."
-            />
-            <svg
-              className="w-5 h-5 absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-            >
-              <path
-                fillRule="evenodd"
-                d="M12.9 14.32a8 8 0 111.42-1.42l4.49 4.49-1.42 1.42-4.49-4.49zM14 8a6 6 0 11-12 0 6 6 0 0112 0z"
-                clipRule="evenodd"
-              />
-            </svg>
           </div>
         </header>
         {/* Main */}
